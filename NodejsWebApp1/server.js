526038533e3451ac80962874ebc72ef6193b7763
unknown <tamar.twena@gmail.com>
2016-05-05 15:24:45 +0300

    initial commit

---
+++ NodejsWebApp1/server.js
@@ -0,0 +1,23 @@
+﻿// Get the packages we need
+var express = require('express');
+
+// Create our Express application
+var app = express();
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+router.get('/', function (req, res) {
+    res.json({ message: 'Hello from demo!' });
+});
+
+// Register all our routes with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Demoe port ' + port);
